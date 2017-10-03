98eb8c3f1b3ac22bdfeabd586f907b99470cabde
alexxxPopa <popa.alex1@gmail.com>
2017-10-03 05:33:16 -0700

    added registration form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import Home from './components/home'
-import SearchList from './components/search_list'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import reducers from './reducers';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
+import SearchList from './components/search_list';
+import Home from './components/home';
+import Register from './components/register';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 class App extends Component {
   render() {
     return (
-     <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/search/:term" component = { SearchList } />
-
-          <Route path="/" component= { Home } />
-        </Switch>
-      </div>
-    </BrowserRouter>
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/search/:term" component={ SearchList } />
+              <Route path="/register" component={ Register } />
+              <Route path="/" component={ Home } />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- src/components/register.js
+++ src/components/register.js
@@ -1,10 +1,91 @@
 // @flow
 
-import React from 'react';
-import AppTemplate from 'components/header/layout';
+import * as React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import Layout from './header/layout';
 
-export default () => (
-  <AppTemplate>
-    <span>This is the Register</span>
-  </AppTemplate>
-);
+
+type ValidationValues = {
+  name: string,
+  email: string,
+  password: string,
+  repeatPassword:string
+}
+
+class RegisterForm extends React.Component<any> {
+
+  renderField(field): React.Node {
+    const { meta: { touched, error } } = field;
+
+    return (
+      <div>
+        <label>{field.label}</label>
+        <input
+          type="text"
+          {...field.input}
+        />
+        <div className="text-help">
+       {touched ? error : ''}
+       </div>
+      </div>
+    );
+  }
+
+  onSubmit(values: ValidationValues): void {
+    console.log(values)
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Name" 
+          name="name"
+          component={this.renderField}
+        />
+        <Field
+          label="Email"
+          name="email"
+          component={this.renderField}
+        />
+        <Field
+          label="Password"
+          name="password"
+          component={this.renderField}
+        />
+        <Field
+          label="Repeat Password"
+          name="repeatPassword"
+          component={this.renderField}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    )
+  }
+}
+
+function validate(values: ValidationValues): any {
+  let errors = {};
+  
+  if (!values.name) {
+    errors.name = "Enter a name!";
+  }
+  if (!values.email) {
+    errors.email = "Enter an email!";
+  }
+  if (!values.password) {
+    errors.password = "Enter a password!";
+  }
+  if (!values.repeatPassword) {
+    errors.repeatPassword = "Enter password repeat!";
+  }
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'RegisterForm'
+})(RegisterForm)
