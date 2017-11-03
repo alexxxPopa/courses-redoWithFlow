5df98569a2abbfc39be2836df4ab7c65c724e6bd
alexxxPopa <alex.popa@mbitcasino.com>
2017-11-03 12:14:38 +0200

    finished facebook signin

--- src/pages/auth/buttons/facebook_signin.js
+++ src/pages/auth/buttons/facebook_signin.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import loadScript from '../../../services/utils';
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import * as signinActions from '../../../modules/auth/signin';
+import { reset } from 'redux-form';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
 
 class FacebookSignin extends Component {
 
-  onClick() {
-    loadScript('https://connect.facebook.net/es_LA/sdk.js', 'facebookOAuth', () => {
+  componentDidMount() {
+    loadScript('https://connect.facebook.net/en_US/sdk.js', 'facebookOAuth', () => {
       window.FB.init({
-        appId: 'todo',
+        appId: '261559787686151',
         autoLogAppEvents: true,
+        status: true,
         xfbml: true, 
-        version: 'v2.9'
+        version: 'v2.10'
       })
-      console.log(1213123)
     })
   }
+
+  onClick() {
+    window.FB.login((response) => {
+       if (response.authResponse && response.status === 'connected') {
+        window.FB.api('/me', {fields: 'email'}, (response)=> {
+          this.props.facebookSignin(response.email, 'facebook')
+            .then( () => this.props.history.push('myPage'))
+        })
+      }  
+  }, {scope: 'public_profile, email'})
+}
 
   render() {
     return (
@@ -22,4 +38,20 @@ class FacebookSignin extends Component {
   }
 }
 
-export default FacebookSignin
+const bindActionsToDispatch = (dispatch) => ({
+  facebookSignin: (email, provider) => {
+    dispatch(showLoading())
+    const promise = dispatch(signinActions.externalSignin(email, provider))
+    promise.then(() => dispatch(hideLoading()))
+          .catch((error) => {
+            dispatch(signinActions.processSigninError(error))
+            dispatch(reset('SigninForm'))
+            dispatch(hideLoading())
+          })
+      return promise;    
+  }
+})
+
+const BoundFacebookSignin = connect(null, bindActionsToDispatch)(FacebookSignin)
+
+export default withRouter(BoundFacebookSignin)
